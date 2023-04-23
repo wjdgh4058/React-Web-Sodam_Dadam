c5012fab9e69d0027257537cd2d72833ee83331b
leejungho <ljhmoa@gmail.com>
2023-04-23 17:52:44 +0900

    feat: NavList Menu 마크업

--- src/components/NavList/NavListItemMenu.tsx
+++ src/components/NavList/NavListItemMenu.tsx
@@ -1,12 +1,12 @@
 function NavListItemMenu({ menuTitle, menuList }: { menuTitle: string; menuList: Array<string> }) {
   return (
-    <div className="w-52">
-      <div className="text-center text-white bg-blue-800">{menuTitle}</div>
+    <div className="w-60">
+      <div className="pt-12 pb-12 text-2xl text-center text-white bg-blue-800 ">{menuTitle}</div>
       {menuList.map((list) => {
         return (
-          <div className="ml-6" key={list}>
+          <button className="pl-6 pt-4 pb-4 border-b w-[100%] text-left text-lg" key={list}>
             {list}
-          </div>
+          </button>
         );
       })}
     </div>
