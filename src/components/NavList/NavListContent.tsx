8b31ff8e97af83662c548c2b766e9f8f7c3d180f
leejungho <ljhmoa@gmail.com>
2023-04-26 16:56:12 +0900

    refactor: 반응형 고려 텍스트 크기 수정

--- src/components/NavList/NavListContent.tsx
+++ src/components/NavList/NavListContent.tsx
@@ -6,7 +6,7 @@ function NavListContent({ itemData }: { itemData: SodaInfoType | ReservationInfo
   return (
     <div className="flex flex-col w-[100%]">
       <div className="flex justify-between pb-4 border-b-2">
-        <div className="text-3xl">{itemData.title}</div>
+        <div className="text-3xl mxl:text-2xl">{itemData.title}</div>
         <NavListCurrentLocation />
       </div>
       {itemData.title === '소담다담 소개' ? <SodaIntroduction sodaData={itemData} /> : null}
