d2224f480a5c1a9ec50c05a7586407635f5492a1
leejungho <ljhmoa@gmail.com>
2023-05-04 22:20:46 +0900

    feat: 퀵 메뉴바 기본 마크업

---
+++ src/components/QuickMenuBar/QuickMenuBar.tsx
@@ -0,0 +1,17 @@
+function QuickMenuBar() {
+  return (
+    <div>
+      <div>Quick Menu</div>
+      <button>
+        <img src="" alt="상담예약 안내 아이콘" />
+        <p>상담예약 안내</p>
+      </button>
+      <button>
+        <img src="" alt="상담,교육 신청 아이콘" />
+        <p>상담/교육 신청</p>
+      </button>
+      <button>TOP ⇧</button>
+    </div>
+  );
+}
+export default QuickMenuBar;
