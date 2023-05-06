5b5e4b026ce46989d03d047c6c316c7f0d3ae709
leejungho <ljhmoa@gmail.com>
2023-05-06 16:56:08 +0900

    feat: 퀵 메뉴바 반응형 스타일

--- src/components/QuickMenuBar/QuickMenuBar.tsx
+++ src/components/QuickMenuBar/QuickMenuBar.tsx
@@ -1,16 +1,24 @@
 function QuickMenuBar() {
   return (
-    <div className="flex flex-col border-2 w-36">
-      <p className="pt-2 pb-2 font-semibold text-center text-white bg-gray-500 ">Quick Menu</p>
-      <button className="flex flex-col items-center pl-6 pr-6 mt-4 ">
-        <img src="../../../public/images/online-booking.png" alt="상담예약 안내 아이콘" className="w-20 h-20 mb-2 " />
-        <p className="pb-2 font-medium border-b-2">상담예약 안내</p>
+    <div className="flex flex-col border-2 w-36 mxl:w-24">
+      <p className="pt-2 pb-2 font-semibold text-center text-white bg-gray-500 mxl:text-sm">Quick Menu</p>
+      <button className="flex flex-col items-center pl-6 pr-6 mt-2 mxl:pl-2 mxl:pr-2">
+        <img
+          src="../../../public/images/online-booking.png"
+          alt="상담예약 안내 아이콘"
+          className="w-20 h-20 mb-2 mxl:border-b-2 mxl:w-16 mxl:h-20 mxl:pb-4"
+        />
+        <p className="pb-2 font-medium border-b-2 mxl:hidden">상담예약 안내</p>
       </button>
-      <button className="flex flex-col items-center pl-6 pr-6 mt-2 ">
-        <img src="../../../public/images/reserved.png" alt="상담,교육 신청 아이콘" className="w-20 h-20 mb-2 " />
-        <p className="pb-2 font-medium">상담/교육 신청</p>
+      <button className="flex flex-col items-center pl-6 pr-6 mt-2 mxl:pl-2 mxl:pr-2">
+        <img
+          src="../../../public/images/reserved.png"
+          alt="상담,교육 신청 아이콘"
+          className="w-20 h-20 mb-2 mxl:w-16 mxl:h-16"
+        />
+        <p className="pb-2 font-medium mxl:hidden">상담/교육 신청</p>
       </button>
-      <button className="pt-2 pb-2 font-medium text-white bg-blue-700">TOP ⇧</button>
+      <button className="pt-2 pb-2 font-medium text-white bg-blue-700 mxl:text-sm ">TOP ⇧</button>
     </div>
   );
 }
