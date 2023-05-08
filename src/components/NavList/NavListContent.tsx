8ce87da011b266943024b28b71947aab92661342
leejungho <ljhmoa@gmail.com>
2023-05-08 21:45:29 +0900

    refactor: console 삭제 및 삼항연산자 && 연산자로 수정

--- src/components/NavList/NavListContent.tsx
+++ src/components/NavList/NavListContent.tsx
@@ -4,15 +4,14 @@ import SodaIntroduction from '../CenterInfo/SodaIntroduction';
 import NavListCurrentLocation from './NavListCurrentLocation';
 
 function NavListContent({ itemData }: { itemData: SodaInfoType | ReservationInfoType | DirectionInfoType }) {
-  console.log(itemData);
   return (
     <div className="flex flex-col w-[100%]">
       <div className="flex justify-between pb-4 border-b-2">
         <div className="text-3xl mxl:text-2xl">{itemData.title}</div>
         <NavListCurrentLocation />
       </div>
-      {itemData.title === '소담다담 소개' ? <SodaIntroduction sodaData={itemData} /> : null}
-      {itemData.title === '상담예약 안내' ? <ReservationInfo reservationData={itemData} /> : null}
+      {itemData.title === '소담다담 소개' && <SodaIntroduction sodaData={itemData} />}
+      {itemData.title === '상담예약 안내' && <ReservationInfo reservationData={itemData} />}
     </div>
   );
 }
