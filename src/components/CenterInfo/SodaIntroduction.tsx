c10c0c5b9e6f9254dad22581e995e6246270acf8
leejungho <ljhmoa@gmail.com>
2023-05-08 22:25:47 +0900

    refactor: 반응형 수정

--- src/components/CenterInfo/SodaIntroduction.tsx
+++ src/components/CenterInfo/SodaIntroduction.tsx
@@ -4,9 +4,9 @@ function SodaIntroduction({ sodaData }: { sodaData: SodaInfoType }) {
   // console.log(sodaData);
   return (
     <div>
-      <div className="mt-12 text-3xl text-blue-900 mxl:text-2xl">{sodaData.headText}</div>
-      <div className="w-[100%] h-96 overflow-hidden mb-12">
-        <img src={sodaData.img} alt={sodaData.alt} className="mt-12" />
+      <div className="mt-12 text-3xl text-blue-900 mxl:text-2xl mmd:mt-6">{sodaData.headText}</div>
+      <div className="w-[100%] h-96 overflow-hidden mb-12 mmd:mb-6">
+        <img src={sodaData.img} alt={sodaData.alt} className="mt-12 mmd:mt-6" />
       </div>
       <ul>
         {sodaData.content.map((item, index) => {
--- src/components/NavList/NavListItemMenu.tsx
+++ src/components/NavList/NavListItemMenu.tsx
@@ -34,17 +34,17 @@ function NavListItemMenu({
     }
   };
   return (
-    <div className="flex flex-col mxl:flex-row mxl:mb-12 mxl:gap-6 mxl:border-b mxl:border-t mxl:border-r">
-      <div className="pt-12 pb-12 text-2xl text-center text-white bg-blue-800 whitespace-nowrap w-60 mxl:text-lg mxl:pt-2 mxl:pb-2 mxl:w-40 mxl:pr-4 mxl:pl-4">
+    <div className="flex flex-col mxl:flex-row mxl:mb-10">
+      <div className="pt-12 pb-12 text-2xl text-center text-white bg-blue-800 whitespace-nowrap mxl:text-base mxl:pt-2 mxl:pb-2 mxl:pr-4 mxl:pl-4 xl:w-60">
         {listData.listName}
       </div>
-      <div className="flex flex-col w-60 mxl:flex-row mxl:w-[100%] mxl:justify-around ">
+      <div className="flex flex-col mxl:flex-row mxl:w-[100%] mxl:justify-around mxl:border-r mxl:border-t mxl:border-b">
         {listItem.map((list) => {
           return (
             <button
               className={`${
                 clickItemName === list ? 'xl:bg-gray-100 text-blue-800' : 'bg-inherit'
-              } pt-4 pb-4 pl-6 text-lg text-left transition duration-500 ease-in-out border-b w-60 mxl:pt-2 mxl:pb-2 mxl:w-28 mxl:text-center mxl:pl-0 mxl:border-b-0 xl:hover:bg-gray-100 hover:text-blue-800`}
+              } pt-4 pb-4 pl-6 text-lg text-left transition duration-500 ease-in-out border-b mxl:pt-2 mxl:pb-2 mxl:text-center mxl:pl-0 mxl:border-b-0 xl:hover:bg-gray-100 hover:text-blue-800 mmd:text-base`}
               key={list}
               onClick={() => clickItem(list)}>
               {list}
