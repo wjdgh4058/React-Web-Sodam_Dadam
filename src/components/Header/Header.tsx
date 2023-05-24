d2aa61ea29711eecb534082ea3fee7f29dcd649e
leejungho <ljhmoa@gmail.com>
2023-05-24 22:17:12 +0900

    refactor: header 하위 메뉴 구조 변경 -> 글자먼저 나타나는 현상 해결

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -41,7 +41,7 @@ function Header() {
   };
   return (
     <header>
-      <div className="flex justify-center mt-4 mb-4">
+      <div className="flex justify-center mt-8 mb-8">
         <Link to={'/'} className="inline-flex flex-row flex-nowrap">
           <img src="../../public/images/sodaLogo.jpeg" alt="소담다담 로고" className="mr-4 w-14 h-14" />
           <div className="flex flex-col items-center justify-center">
@@ -56,18 +56,37 @@ function Header() {
         </Link>
       </div>
       <ul
-        className="pt-2 pb-2 border-t border-b border-stone-950 mb-14"
+        className="pt-4 pb-4 border-t border-b border-stone-950 mb-14 mxl:pt-2 mxl:pb-2"
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}>
-        <div className="relative flex ml-auto mr-auto max-w-7xl">
+        <div className="relative flex ml-auto mr-auto max-w-screen-2xl ">
           {headerListData.map((item, index) => (
-            <HeaderNavList headerList={item} key={index} menuVisible={menuVisible} />
+            <HeaderNavList headerList={item} key={index} />
           ))}
         </div>
         <div
-          className={`absolute w-full mt-2 bg-blue-500 ${
+          className={`absolute w-full mt-4 bg-blue-500 mxl:mt-2 ${
             menuVisible ? 'h-40' : 'h-0'
-          } transition-height duration-[400ms] ease-in-out overflow-hidden z-10`}></div>
+          } transition-height duration-[400ms] ease-in-out overflow-hidden z-10`}>
+          <div className="flex ml-auto mr-auto max-w-screen-2xl">
+            {headerListData.map((item, index) => {
+              return (
+                <div className="inset-x-0 z-20 flex flex-col h-0 gap-2 mt-5 basis-1/5" key={index}>
+                  {item.list.map((listName, index) => {
+                    return (
+                      <Link
+                        to={`${item.link}/${index + 1}`}
+                        key={index}
+                        className="flex justify-center p-1 ml-auto mr-auto text-xl font-normal text-white transition duration-300 ease-in-out hover:text-blue-700 hover:border-b hover:text-2xl mxl:hover:text-base mxl:text-base">
+                        {listName}
+                      </Link>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </ul>
     </header>
   );
--- src/components/Header/HeaderNavList.tsx
+++ src/components/Header/HeaderNavList.tsx
@@ -1,28 +1,14 @@
 import { Link } from 'react-router-dom';
 import { IHeaderType } from './HeaderType';
 
-function HeaderNavList({ headerList, menuVisible }: { headerList: IHeaderType; menuVisible: boolean }) {
+function HeaderNavList({ headerList }: { headerList: IHeaderType }) {
   return (
-    <li className="relative text-xl font-medium mxl:text-base basis-1/5 ">
+    <li className="text-2xl mxl:text-lg basis-1/5 ">
       <Link
         className="flex justify-center transition duration-500 ease-in-out pr-auto pl-auto hover:text-blue-800"
         to={`${headerList.link}/1`}>
         {headerList.name}
       </Link>
-      <div>
-        <div className={`absolute inset-x-0 z-20 flex flex-col h-0 gap-2 mt-5 max-w-7xl ${menuVisible || 'hidden'}`}>
-          {headerList.list.map((list, index) => {
-            return (
-              <Link
-                to={`${headerList.link}/${index + 1}`}
-                key={index}
-                className="p-1 ml-auto mr-auto text-lg font-normal text-white transition duration-300 ease-in-out hover:text-blue-700 hover:border-b hover:text-xl mxl:hover:text-lg mxl:text-base">
-                {list}
-              </Link>
-            );
-          })}
-        </div>
-      </div>
     </li>
   );
 }
