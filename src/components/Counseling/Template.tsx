9f41ce13500929bcbf5b5713d0451066b0c7432c
leejungho <ljhmoa@gmail.com>
2023-06-09 14:26:10 +0900

    feat: 반응형 고려 text-size 설정

--- src/components/Counseling/Template.tsx
+++ src/components/Counseling/Template.tsx
@@ -4,38 +4,38 @@ import { ICounselingType } from './CounselingType';
 function Template({ data }: { data: ICounselingType }) {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col gap-4 mt-32 ">
+      <div className="flex flex-col gap-4 mt-32 mxl:mt-20">
         {data.mainText.map((item, index) => {
           return (
-            <p className="ml-auto mr-auto text-4xl font-medium " key={index}>
+            <p className="ml-auto mr-auto text-4xl font-medium mxl:text-2xl" key={index}>
               {item}
             </p>
           );
         })}
       </div>
-      <div className="flex flex-col gap-2 mt-32">
+      <div className="flex flex-col gap-2 mt-32 mxl:mt-20">
         {data.subText_1.map((item, index) => {
           return (
-            <p className="ml-auto mr-auto text-3xl text-blue-800" key={index}>
+            <p className="ml-auto mr-auto text-3xl text-blue-800 mxl:text-xl" key={index}>
               {item}
             </p>
           );
         })}
       </div>
       <div
-        className="w-[80%] h-80 mt-32 ml-auto mr-auto bg-center bg-cover"
+        className="w-[80%] h-80 mt-32 ml-auto mr-auto bg-cover bg-center mxl:mt-20"
         style={{ backgroundImage: `url(${data.img_1})` }}></div>
-      <div className="flex flex-col gap-2 mt-40">
+      <div className="flex flex-col gap-2 mt-40 mxl:mt-28">
         {data.subText_2.map((item, index) => {
           return (
-            <p className="ml-auto mr-auto text-2xl " key={index}>
+            <p className="ml-auto mr-auto text-2xl mxl:text-xl" key={index}>
               {item}
             </p>
           );
         })}
       </div>
       <div
-        className="w-[80%] h-80 mt-40 ml-auto mr-auto bg-center bg-cover mb-28"
+        className="w-[80%] h-80 mt-40 ml-auto mr-auto bg-cover bg-center mb-28 mxl:mt-28"
         style={{ backgroundImage: `url(${data.img_2})` }}></div>
       <ReservationBtn />
     </div>
