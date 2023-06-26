6213d6859bc8b7670562d7f4ea45ebf970044f90
leeuihyun <meta212121@naver.com>
2023-06-26 19:00:37 +0900

    feat : react-query 기본 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,23 @@ import App from './App';
 import '../styles/global.css';
 import { BrowserRouter } from 'react-router-dom';
 import initMocks from './mocks';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 if (process.env.NODE_ENV === 'development') {
   await initMocks();
 }
-
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
 );
