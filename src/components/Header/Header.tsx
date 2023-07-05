aae7b6210764339fcac5584f3fdc4ec58f8aa99e
leejungho <ljhmoa@gmail.com>
2023-07-05 21:49:28 +0900

    refactor: header 크기 재조정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,43 +24,47 @@ function Header() {
     <>
       {listData && (
         <header className="sticky top-[-90px] bg-white z-10">
-          <div className="flex justify-center mt-8 mb-8">
+          <div className="flex justify-center mt-4 mb-4 mlg:mt-2 mlg:mb-2">
             <Link to={'/'} className="inline-flex flex-row flex-nowrap">
-              <img src="../../public/images/sodaLogo.jpeg" alt="소담다담 로고" className="mr-4 w-14 h-14" />
+              <img
+                src="../../public/images/sodaLogo.jpeg"
+                alt="소담다담 로고"
+                className="w-10 h-10 mr-4 mlg:w-8 mlg:h-8 mlg:mr-2"
+              />
               <div className="flex flex-col items-center justify-center">
                 <div className="flex">
-                  <p className="mr-2 text-2xl font-bold text-blue-900 mxl:text-xl">소담다담</p>
-                  <p className="text-2xl font-semibold text-gray-500 mxl:text-xl">심리상담센터</p>
+                  <p className="mr-2 font-bold text-blue-900 mlg:text-sm">소담다담</p>
+                  <p className="font-semibold text-gray-500 mlg:text-sm">심리상담센터</p>
                 </div>
                 <div>
-                  <p className="text-sm text-gray-400 mxl:text-xs">SODA COUNSELING CENTER</p>
+                  <p className="text-xs text-gray-400 mxl:text-xs">SODA COUNSELING CENTER</p>
                 </div>
               </div>
             </Link>
           </div>
           <ul
-            className="pt-4 pb-4 border-t border-b border-stone-950 mb-14 mxl:pt-2 mxl:pb-2"
+            className="pt-2 pb-2 mb-8 border-t border-b border-stone-950 mlg:pt-1 mlg:pb-1 mlg:mb-8"
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}>
-            <div className="relative flex ml-auto mr-auto max-w-screen-2xl ">
+            <div className="relative flex max-w-screen-xl ml-auto mr-auto ">
               {listData.slice(0, 5).map((item, index) => (
                 <HeaderNavList headerList={item} key={index} />
               ))}
             </div>
             <div
-              className={`absolute w-full mt-4 bg-blue-500 mxl:mt-2 ${
-                menuVisible ? 'h-40' : 'h-0'
+              className={`absolute w-full mt-2 bg-blue-500 mlg:mt-1 ${
+                menuVisible ? 'h-40 mlg:h-32' : 'h-0'
               } transition-height duration-[400ms] ease-in-out overflow-hidden z-10`}>
-              <div className="flex ml-auto mr-auto max-w-screen-2xl">
+              <div className="flex max-w-screen-xl ml-auto mr-auto">
                 {listData.slice(0, 5).map((item, index) => {
                   return (
-                    <div className="inset-x-0 z-20 flex flex-col h-0 gap-2 mt-4 basis-1/5" key={index}>
-                      {item.listItem.slice(0, 3).map((listName, index) => {
+                    <div className="inset-x-0 z-20 flex flex-col h-0 gap-2 mt-3 basis-1/5 mlg:mt-2" key={index}>
+                      {item.listItem.slice(0, 5).map((listName, index) => {
                         return (
                           <Link
                             to={`${item.url}/${index + 1}`}
                             key={index}
-                            className="flex justify-center p-1 ml-auto mr-auto text-xl font-normal text-white transition duration-300 ease-in-out hover:text-blue-900 hover:border-b mxl:text-base">
+                            className="flex justify-center ml-auto mr-auto text-sm font-normal text-white transition duration-300 ease-in-out hover:text-blue-900 hover:border-b mlg:text-xs">
                             {listName}
                           </Link>
                         );
--- src/components/Header/HeaderNavList.tsx
+++ src/components/Header/HeaderNavList.tsx
@@ -3,7 +3,7 @@ import { INavListDataType } from '../NavList/NavListType';
 
 function HeaderNavList({ headerList }: { headerList: INavListDataType }) {
   return (
-    <li className="text-2xl mxl:text-lg basis-1/5 ">
+    <li className=" mlg:text-sm basis-1/5">
       <Link
         className="flex justify-center transition duration-500 ease-in-out pr-auto pl-auto hover:text-blue-800"
         to={`${headerList.url}/1`}>
