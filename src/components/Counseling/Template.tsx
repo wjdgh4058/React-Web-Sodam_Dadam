588821b33cdf28d4df7886ec03f8b6f27fec1694
leejungho <ljhmoa@gmail.com>
2023-07-07 15:50:02 +0900

    refactor: 프로그램 상세 페이지 크기 재조정

--- src/components/Counseling/Template.tsx
+++ src/components/Counseling/Template.tsx
@@ -4,38 +4,38 @@ import { ICounselingType } from './CounselingType';
 function Template({ data }: { data: ICounselingType }) {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col gap-4 mt-32 mxl:mt-20">
+      <div className="flex flex-col gap-4 mt-16 mmd:mt-12">
         {data.mainText.map((item, index) => {
           return (
-            <p className="ml-auto mr-auto text-4xl font-medium mxl:text-2xl" key={index}>
+            <p className="ml-auto mr-auto text-xl font-medium mmd:text-lg" key={index}>
               {item}
             </p>
           );
         })}
       </div>
-      <div className="flex flex-col gap-2 mt-32 mxl:mt-20">
+      <div className="flex flex-col gap-2 mt-16 mmd:mt-12">
         {data.subText_1.map((item, index) => {
           return (
-            <p className="ml-auto mr-auto text-3xl text-blue-800 mxl:text-xl" key={index}>
+            <p className="ml-auto mr-auto text-lg text-blue-800 mmd:text-base" key={index}>
               {item}
             </p>
           );
         })}
       </div>
       <div
-        className="w-[80%] h-80 mt-32 ml-auto mr-auto bg-cover bg-center mxl:mt-20"
+        className="w-[80%] h-60 mt-16 ml-auto mr-auto bg-cover bg-center mmd:mt-12 max-w-lg mmd:h-40"
         style={{ backgroundImage: `url(${data.img_1})` }}></div>
-      <div className="flex flex-col gap-2 mt-40 mxl:mt-28">
+      <div className="flex flex-col gap-2 mt-16 mmd:mt-12">
         {data.subText_2.map((item, index) => {
           return (
-            <p className="ml-auto mr-auto text-2xl mxl:text-xl" key={index}>
+            <p className="ml-auto mr-auto text-base mmd:text-sm" key={index}>
               {item}
             </p>
           );
         })}
       </div>
       <div
-        className="w-[80%] h-80 mt-40 ml-auto mr-auto bg-cover bg-center mb-28 mxl:mt-28 mxl:mb-8"
+        className="w-[80%] h-60 mt-16 ml-auto mr-auto bg-cover bg-center mb-16 mmd:mt-12 mmd:mb-8 max-w-lg mmd:h-40"
         style={{ backgroundImage: `url(${data.img_2})` }}></div>
       <ReservationBtn />
     </div>
--- src/components/ReservationBtn/ReservationBtn.tsx
+++ src/components/ReservationBtn/ReservationBtn.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 function ReservationBtn() {
   return (
     <>
-      <p className="pb-4 mt-4 border-b mmd:text-sm">모든 상담 및 프로그램은 사전 예약제로 운영됩니다.</p>
-      <p className="pb-4 mt-4 border-b mmd:text-sm">
+      <p className="pb-4 mt-4 text-sm border-b mmd:text-xs">모든 상담 및 프로그램은 사전 예약제로 운영됩니다.</p>
+      <p className="pb-4 mt-4 text-sm border-b mmd:text-xs">
         예약 없는 방문은 어려우니, 미리 전화로 예약 문의를 해주시면 안내해 드립니다. 전화 연결이 안 될 때는 상담 진행
         중일 수 있으니, 문자 메시지를 남겨 주시면 상담 종료 후에 연락 드리겠습니다.
       </p>
-      <p className="pb-4 mt-4 font-light border-b mmd:text-sm">
+      <p className="pb-4 mt-4 text-sm font-light border-b mmd:text-xs">
         * 상담 내용은 철저한 비밀 보장이 되며, 내담자분의 안전을 보장하기 위한 조치가 제 1순위로 이루어집니다.{' '}
       </p>
       <div className="flex justify-center my-8 mmd:my-4">
